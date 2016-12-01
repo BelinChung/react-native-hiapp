4ce50c5879ae8672846e50131659f401e7751d99
BelinChung <belinchung@gmail.com>
2016-12-01 15:06:26 +0800

    Update dependencies

--- android/app/src/main/java/com/rn_hiapp/MainApplication.java
+++ android/app/src/main/java/com/rn_hiapp/MainApplication.java
@@ -4,13 +4,12 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new ExtraDimensionsPackage()
+            new ExtraDimensionsPackage()
       );
     }
   };
